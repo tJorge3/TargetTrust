a82bfa5f9dd18cc376d9e6fb277f5513a05cbb6a
tJorge3 <tjorge493493@gmail.com>
2024-08-16 13:37:39 -0300

    aula01

---
+++ aula01/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int valor = somatorio(5,12);
+        System.out.println(valor);
+        System.out.println(valor);
+    }
+
+    public static int somatorio(int a, int b) {
+        return a + b;
+    }
+}
